02b4ffbfa877773fa9bb78cb2896f4696c66139b
manighahrmani <mashapicasso@gmail.com>
2022-03-28 21:57:29 +0100

    Add code from worksheet-1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
+use std::io;
+
 fn main() {
   const KITTEN: &str = "🐱";
-  println!("Hi! {}", KITTEN);
+  println!("Hi from {}!", KITTEN);
 
-  let mut number_of_files = 1;
-  println!("numberOfFiles is {}", number_of_files);
-  number_of_files = 2;
-  println!("x is now {}", number_of_files);
+  let number_of_files: u32 = 1;
 
-  println!("Bye! {}", KITTEN);
+  println!("How many files would you like to open?");
+
+  let mut input = String::new();
+  match io::stdin().read_line(&mut input) {
+    Ok(n) => {
+      println!("{} bytes read", n);
+      println!("Your input was: ({})", input);
+    }
+    Err(error) => println!("Error while reading your input: {}", error),
+  }
+
+  //  let mut input = String::new();
+  //  if let Err(e) = io::stdin().read_line(&mut input) {
+  //   println!("Error while reading your input: {}", e);
+  // } else {
+  //   println!("Your input was: ({})", input);
+  // }
+
+  // let mut input = String::new();
+  //  io::stdin()
+  //  .read_line(&mut input)
+  //  .expect("Error while reading your input!");
+  // println!("Your input was: ({})", input);
+
+  println!("We need to open {} files.", number_of_files);
+
+  println!("Bye from {}!", KITTEN);
 }
