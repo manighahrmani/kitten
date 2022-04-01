4d4c84a4c765dbd4045e7543a1a9c7fb8b755f27
manighahrmani <mashapicasso@gmail.com>
2022-04-01 20:54:47 +0100

    Update main.rs. Parsing input and unwrapping.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,29 @@ fn main() {
   const KITTEN: &str = "🐱";
   println!("Hi from {}!", KITTEN);
 
-  let number_of_files: u32 = 1;
+  let number_of_files: u32;
 
   println!("How many files would you like to open?");
 
   let mut input: String = String::new();
   io::stdin()
     .read_line(&mut input)
-    .expect("Error while reading your input!");
+    .expect("Error while reading your input!"); 
+  // on error, the program will panic and crash
 
   input = input.trim().to_string();
   println!("Your input was: ({})", input);
 
+  // on parsin errors, no error message will be printed
+  number_of_files = input.parse().unwrap_or(1);
+  // match input.parse::<u32>() {
+  //   Ok(num) => {
+  //     number_of_files = num;
+  //   }
+  //   Err(e) => {
+  //     println!("Error on parsing input: {}", e);
+  //   }
+  // }
   println!("We need to open {} files.", number_of_files);
 
   println!("Bye from {}!", KITTEN);
