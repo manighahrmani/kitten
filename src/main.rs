e44da0658c027291c060fb4582517da029348e9c
manighahrmani <mashapicasso@gmail.com>
2022-04-01 10:12:47 +0100

    Update main.rs
    Add a comment pointing our that number_of_files is immutable for now. In the next branch, its content must be updated.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
   const KITTEN: &str = "🐱";
   println!("Hi from {}!", KITTEN);
 
-  let number_of_files: u32 = 1;
+  let number_of_files: u32 = 1; // immutable for now
 
   println!("How many files would you like to open?");
 
