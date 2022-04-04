718a06d6f50831d14a1a43781e44813adab0d0ea
manighahrmani <mashapicasso@gmail.com>
2022-04-05 00:21:38 +0100

    Add challenge code to main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ fn main() {
   match io::stdin().read_line(&mut input) {
     Ok(_) => {
       input.pop();
-      println!("Your input was: ({})", input);
     }
     Err(error) => {
       panic!("Error while reading your input: {}", error);
@@ -31,9 +30,29 @@ fn main() {
     }
   }
 
-  for i in 1..number_of_files {
-    println!("Please enter the name/path to a file {}: ", i);
-    // need to ask for an input here
+  for i in 1..(number_of_files + 1) {
+    let ordinal = match i {
+      i if i % 10 == 1 && i != 11 => "st",
+      i if i % 10 == 2 && i != 12 => "nd",
+      i if i % 10 == 3 && i != 13 => "rd",
+      _ => "th",
+    };
+
+    match number_of_files - i {
+      0 => println!("Please enter the name/path to the last file:"),
+      n => {
+        let singular_or_plural = if n == 1 { "filename" } else { "filenames" };
+        println!(
+          "{} more {} to go.\nPlease enter the name/path to the {}{} file: ",
+          number_of_files - i,
+          singular_or_plural,
+          i,
+          ordinal
+        );
+      }
+    };
+
+    // need to ask for input here
   }
 
   println!("Bye from {}!", KITTEN);
