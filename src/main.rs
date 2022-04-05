07819fc15b85e6720bffadfb39f42247084db6d5
manighahrmani <mashapicasso@gmail.com>
2022-04-05 20:43:26 +0100

    Update main.rs. Add a return str function and a return String.

--- src/main.rs
+++ src/main.rs
@@ -25,26 +25,7 @@ fn main() {
   number_of_files = parse_number_of_files(&input).unwrap();
 
   for i in 1..(number_of_files + 1) {
-    let ordinal = match i {
-      i if i % 10 == 1 && i != 11 => "st",
-      i if i % 10 == 2 && i != 12 => "nd",
-      i if i % 10 == 3 && i != 13 => "rd",
-      _ => "th",
-    };
-
-    match number_of_files - i {
-      0 => println!("Please enter the name/path to the last file:"),
-      n => {
-        let singular_or_plural = if n == 1 { "filename" } else { "filenames" };
-        println!(
-          "{} more {} to go.\nPlease enter the name/path to the {}{} file: ",
-          number_of_files - i,
-          singular_or_plural,
-          i,
-          ordinal
-        );
-      }
-    };
+    println!("{}", get_filename_query(i, number_of_files));
 
     // need to ask for input here
   }
@@ -52,7 +33,40 @@ fn main() {
   println!("Bye from {}!", KITTEN);
 }
 
-fn get_first_word(input: String) -> String {
+fn ordinal_value(number: u32) -> String {
+  number.to_string()
+    + match number % 10 {
+      1 if number != 11 => "st",
+      2 if number != 12 => "nd",
+      3 if number != 13 => "rd",
+      _ => "th",
+    }
+}
+
+fn singular_or_plural(number: u32) -> &'static str {
+  if number == 1 {
+    "file"
+  } else {
+    "files"
+  }
+}
+
+fn get_filename_query(number_of_file: u32, total_number_of_files: u32) -> String {
+  let query = match total_number_of_files - number_of_file {
+    0 => String::from("Please enter the name/path to the last file:"),
+    number_of_files_left => {
+      format!(
+        "{} more filename{} to go.\nPlease enter the name/path to the {} file: ",
+        number_of_files_left,
+        singular_or_plural(number_of_file),
+        ordinal_value(number_of_file)
+      )
+    }
+  };
+  query
+}
+
+fn _get_first_word(input: String) -> String {
   let mut first_word = String::new();
   for c in input.chars() {
     if c == ' ' {
@@ -63,7 +77,7 @@ fn get_first_word(input: String) -> String {
   first_word
 }
 
-fn keep_only_first_word(input: String) -> String {
+fn _keep_only_first_word(input: String) -> String {
   let mut words = input.split_whitespace();
   let first_word = words.next().unwrap();
   first_word.to_string()
