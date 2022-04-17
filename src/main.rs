c26b62cbde2551b89b31c81a1c02c456cce554bb
manighahrmani <mashapicasso@gmail.com>
2022-04-17 17:32:46 +0100

    Comment hard way and update easy way with args

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,50 @@ fn main() {
   println!("Hi from {}!", KITTEN);
 
   let passed_args: env::Args = env::args();
-  let filenames: Vec<String> = arguments::parse(passed_args).unwrap().orphans;
+  let filenames: Vec<String>;
+  match arguments::parse(passed_args) {
+    Ok(args) => {
+      filenames = args.orphans;
+    }
+    Err(error) => {
+      panic!("Error while parsing your input: {}", error);
+    }
+  }
 
   // TODO: Add a check for help function
 
+  // let output: String =
+  //   filenames
+  //     .iter()
+  //     .enumerate()
+  //     .fold(String::new(), |accumulator, (index, filename)| {
+  //         println!(
+  //           "{} file is being processed: {}",
+  //           string_helper::as_ordinal(index as u32),
+  //           filename
+  //         );
+  //       let file_content: String = file_helper::file_content(filename).unwrap_or_else(|error| {
+  //         panic!(
+  //           "Error while reading file {}: {}",
+  //           filename,
+  //           error
+  //         );
+  //       });
+  //       accumulator + &file_content
+  //     });
+
   let mut output = String::new();
 
-  for index_of_filename in 0..filenames.len() {
-    let filename: &str = filenames.get(index_of_filename).unwrap();
+  for index in 0..filenames.len() {
+    let filename: &str = filenames[index].as_str();
 
     println!(
       "{} file is being processed: {}",
-      string_helper::as_ordinal(index_of_filename as u32),
+      string_helper::as_ordinal(index as u32),
       filename
     );
 
-    let file_content_result: Result<String, String> = file_helper::file_content(filename);
-    match file_content_result {
+    match file_helper::file_content(filename){
       Ok(file_content) => {
         output += &file_content;
       }
