0ae4df7566e81605506cacf007a9833b20c797a3
manighahrmani <mashapicasso@gmail.com>
2022-04-18 00:31:55 +0100

    Uncomment the chaining version

--- src/main.rs
+++ src/main.rs
@@ -18,46 +18,42 @@ fn main() {
 
   // TODO: Add a check for help function
 
-  // let output: String =
-  //   filenames
-  //     .iter()
-  //     .enumerate()
-  //     .fold(String::new(), |accumulator, (index, filename)| {
-  //         println!(
-  //           "{} file is being processed: {}",
-  //           string_helper::as_ordinal(index as u32),
-  //           filename
-  //         );
-  //       let file_content: String = file_helper::file_content(filename).unwrap_or_else(|error| {
-  //         panic!(
-  //           "Error while reading file {}: {}",
-  //           filename,
-  //           error
-  //         );
-  //       });
-  //       accumulator + &file_content
-  //     });
+  let output: String =
+    filenames
+      .iter()
+      .enumerate()
+      .fold(String::new(), |accumulator, (index, filename)| {
+        println!(
+          "{} file is being processed: {}",
+          string_helper::as_ordinal(index as u32),
+          filename
+        );
+        let file_content: String = file_helper::file_content(filename).unwrap_or_else(|error| {
+          panic!("Error while reading file {}: {}", filename, error);
+        });
+        accumulator + &file_content
+      });
 
-  let mut output = String::new();
+  // let mut output = String::new();
 
-  for index in 0..filenames.len() {
-    let filename: &str = filenames[index].as_str();
+  // for index in 0..filenames.len() {
+  //   let filename: &str = filenames[index].as_str();
 
-    println!(
-      "{} file is being processed: {}",
-      string_helper::as_ordinal((index + 1) as u32),
-      filename
-    );
+  //   println!(
+  //     "{} file is being processed: {}",
+  //     string_helper::as_ordinal((index + 1) as u32),
+  //     filename
+  //   );
 
-    match file_helper::file_content(filename) {
-      Ok(file_content) => {
-        output += &file_content;
-      }
-      Err(error) => {
-        panic!("Error while reading file {}: {}", filename, error);
-      }
-    }
-  }
+  //   match file_helper::file_content(filename) {
+  //     Ok(file_content) => {
+  //       output += &file_content;
+  //     }
+  //     Err(error) => {
+  //       panic!("Error while reading file {}: {}", filename, error);
+  //     }
+  //   }
+  // }
 
   println!("Here is the output:\n{}", output);
 
